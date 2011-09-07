ad5516d2f6f7b08239d1cb6ff14f90c31718a0fb
Teresa Miyar <teresa.miyar@magnolia-cms.com>
2011-09-07 09:27:31 +0000

    shop: add new control for the teaser that will get the shopname of the current site
    git-svn-id: file:///home/gjoseph/subversion-migration/svn-repo-copy/forge/magnolia-shop/trunk@49015 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ src/main/java/info/magnolia/module/shop/dialog/TreeWebsiteProductListUuidControl.java
@@ -0,0 +1,71 @@
+/**
+ * This file Copyright (c) 2003-2011 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.module.shop.dialog;
+
+import javax.jcr.RepositoryException;
+
+import info.magnolia.cms.core.Content;
+import info.magnolia.cms.gui.dialog.DialogUUIDLink;
+import info.magnolia.cms.util.NodeDataUtil;
+import info.magnolia.context.Context;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.module.shop.util.ShopUtil;
+import info.magnolia.module.templatingkit.util.STKUtil;
+
+/**
+ * This control will set the shop name used in the current site, by doing this,
+ * we get the desired products in the productlist tree.
+ * @author tmiyar
+ *
+ */
+public class TreeWebsiteProductListUuidControl extends DialogUUIDLink {
+
+    @Override
+    protected void doBeforeDrawHtml() {
+        super.doBeforeDrawHtml();
+        try {
+            Content content = this.getStorageNode().getAncestor(1);
+        
+            Content shopHome = STKUtil.getContentByTemplateCategorySubCategory(content, "feature", "shopHome");
+            if(shopHome != null) {
+                MgnlContext.setAttribute(ShopUtil.ATTRIBUTE_SHOPNAME, NodeDataUtil.getString(shopHome, "currentShop"), Context.SESSION_SCOPE);
+            }
+        } catch (RepositoryException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+        
+}
