cb845458081eb8d0a4dd49d65ffb5c8df3316330
Milan Divilek <milan.divilek@magnolia-cms.com>
2015-08-10 11:56:19 +0200

    Revert "MSHOP-182: Fixed install tasks in compliant with unit test cases." This reverts commit 18dc65ccfe71848d5a72a7e4fe3f6579eedb0650.
    Revert "MSHOP-181: Escape product categories names on the product detail dialog"
    This reverts commit 23dfd481ae9c210f9622438e137be497bee3fc6b.
    
    Revert "MSHOP-182: Fixed so we will use node name in case of empty page title."
    This reverts commit 7daa4a373e0c1a877d5f6e1fbbf6a64469ae1f09.
    
    Revert "MSHOP-180: Fix the empty/missing supplier field selection when creating"
    This reverts commit 3d00a900d6eee1f471b816af337fc1fbcf691bd1.

--- magnolia-module-shop/src/main/java/info/magnolia/shop/app/field/factory/ProductCategoriesFieldFactory.java
+++ magnolia-module-shop/src/main/java/info/magnolia/shop/app/field/factory/ProductCategoriesFieldFactory.java
@@ -76,7 +76,7 @@ public class ProductCategoriesFieldFactory extends OptionGroupFieldFactory {
 
                 for (Node category : shopCategories) {
                     SelectFieldOptionDefinition field = new SelectFieldOptionDefinition();
-                    field.setLabel(PropertyUtil.getString(category, "title", category.getName()));
+                    field.setLabel(PropertyUtil.getString(category, "title"));
                     field.setValue(category.getIdentifier());
                     res.add(field);
                 }
